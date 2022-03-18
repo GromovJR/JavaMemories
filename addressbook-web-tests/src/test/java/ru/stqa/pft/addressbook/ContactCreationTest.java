c5c388280e750c791f5948b7a03680157a2664e4
Igor Savchenko <43295464+GromovJR@users.noreply.github.com>
2022-03-18 16:55:24 +0300

    Add GroupDeletionTest + TestBase class + minor fix

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -8,12 +8,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class ContactCreationTest {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/index.php");
-    login();
+    login("admin", "secret");
   }
 
   @Test
@@ -24,19 +24,19 @@ public class ContactCreationTest {
     goToHomePage();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     logout();
     wd.quit();
   }
 
 
-  private void login() {
+  private void login(String login, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(login);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).submit();
   }
 
